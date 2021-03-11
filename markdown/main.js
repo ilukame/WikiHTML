2b3f82896200b73dcc0cae396722126edde79da0
ilukame <22080768+ilukame@users.noreply.github.com>
2021-03-12 00:01:26 +0900

    Add files via upload

--- markdown/main.js
+++ markdown/main.js
@@ -6,7 +6,7 @@ $(function(){
         let url_char = $(this).attr('data-char_url')
         if (url_char === "change1") document.right.location.href = "main.html";
         if (url_char === "change2") document.right.location.href = "main.html";
-        if (url_char === "はじめに") document.right.location.href = "TOPページ/はじめに/はじめに.html";
+        if (url_char === "はじめに") document.right.location.href = "Pages/TOP/はじめに/はじめに.html";
     })
     .mouseover(function(){
       $(this).css('background', '#c00');
